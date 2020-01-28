fbfbee4d43dce12ab120a57908894b7dbcb2ada6
gyaan <gyaan1334@gmail.com>
2020-01-28 17:25:24 +0530

    docker build for project

--- internal/config/config.go
+++ internal/config/config.go
@@ -19,6 +19,8 @@ type Config struct {
 	ApplicationPort           string `json:"application_port" mapstructure:"application_port"`
 	MongoDbConnectionUrl      string `json:"mongo_db_connection_url" mapstructure:"mongo_db_connection_url"`
 	MongoDatabaseName         string `json:"mongo_database_name" mapstructure:"mongo_database_name"`
+	MongoDatabaseUserName     string `json:"mongo_database_user_name" mapstructure:"mongo_database_username"`
+	MongoDatabasePassword     string `json:"mongo_database_password" mapstructure:"mongo_database_password"`
 	ShortUrlExpiryTime        int    `json:"short_url_expiry_time" mapstructure:"short_url_expiry_time"` // time in minutes
 	BaseUrl                   string `json:"base_url" mapstructure:"base_url"`
 	MinimumShortUrlIdentifier int    `json:"minimum_short_url_identifier" mapstructure:"minimum_short_url_identifier"`
@@ -41,6 +43,23 @@ func New() *Config {
 	if err != nil {
 		log.Fatalf("%v", err)
 	}
+
+	//set mongo env variable default config values will overwrite with env variable
+	err = viper.BindEnv("mongo_database_name", "MONGO_INITDB_DATABASE")
+	if err != nil {
+		log.Printf("not able to set env variable for mongo_database_name %v", err)
+	}
+
+	err = viper.BindEnv("mongo_database_username", "MONGO_INITDB_ROOT_USERNAME")
+	if err != nil {
+		log.Printf("not able to set env variable for mongo_database_username %v", err)
+	}
+
+	err = viper.BindEnv("mongo_database_password", "MONGO_INITDB_ROOT_PASSWORD")
+	if err != nil {
+		log.Printf("not able to set env variable for mongo_database_password %v", err)
+	}
+
 	return conf
 }
 
